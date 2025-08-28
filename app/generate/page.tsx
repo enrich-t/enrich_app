356cf24784136b1d1bd7756586e60556ce3ac858
enrich-t <enrichlaunch@gmail.com>
2025-08-28 16:58:24 -0600

    fix(generate): wrap page with ToastProvider to satisfy useToast context

--- app/generate/page.tsx
+++ app/generate/page.tsx
@@ -1,7 +1,12 @@
 export const dynamic = 'force-dynamic';
 
 import GenerateClient from '../../components/generate/GenerateClient';
+import { ToastProvider } from '../../components/Toast';
 
 export default function Page() {
-  return <GenerateClient />;
+  return (
+    <ToastProvider>
+      <GenerateClient />
+    </ToastProvider>
+  );
 }
