f39039edc2cb00fbcd8d3929e4c64456733d3b32
enrich-t <enrichlaunch@gmail.com>
2025-08-29 09:48:32 -0600

    layout: wrap app with Shell + ToastProvider to restore global sidebar and toast context

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import './globals.css';
 import React from 'react';
+import Shell from '../components/Shell';
 import { ToastProvider } from '../components/Toast';
 
 export const metadata = {
@@ -11,11 +12,10 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en">
       <body>
-        <ToastProvider>{children}</ToastProvider>
+        <ToastProvider>
+          <Shell>{children}</Shell>
+        </ToastProvider>
       </body>
     </html>
   );
 }
-
-
-
